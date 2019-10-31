3dcefeb756079621626a048f583fdee2a89be009
chanmi <cksal5911@naver.com>
2019-11-01 00:45:40 +0900

    test

--- src/component/CheckBoxGroup.js
+++ src/component/CheckBoxGroup.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import NodeTreeModel from "./Model/NodeTreeModel";
 
 import "./CheckBoxGroup.scss";
 
 const CheckBoxGroup = ({ nodes, className, checkList }) => {
-  const [checkReference, setCheckReference] = useState(checkList);
   const model = new NodeTreeModel();
   model.flattenNodes(nodes);
+
+  useEffect(() => {});
+
+  const [checkReference, setCheckReference] = useState(checkList);
   const treeNode = nodes => {
     const mapNodes = nodes.map(node => {
       const flatNode = model.getNode(node.value);
@@ -15,14 +18,15 @@ const CheckBoxGroup = ({ nodes, className, checkList }) => {
       const children = flatNode.hasChildrenNode
         ? treeNode(node.children)
         : null;
-      const onChange = e => {
+      const onChange = () => {
         checkReference.includes(node.value)
           ? setCheckReference(
               checkReference.filter(
                 checkReference => checkReference !== node.value
               )
             )
-          : checkReference.push(node.value);
+          : setCheckReference(checkReference.concat(node.value));
+
         console.log(checkReference);
       };
       return (
